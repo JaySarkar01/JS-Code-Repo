a9da81bbfd08ae84f3b09539317919b91e5d7d6c
Jay Sarkar <jaisarkari9@gmail.com>
2024-10-24 08:41:49 +0000

    dataTypes in js

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,13 @@
+let numberValue = 1223;
+let bigIntValue =873271462896127n;
+let stringValue ="jay sarkar";
+let booleanValue =true;
+let nullValue = null;
+let undefined;
+
+console.table([numberValue,bigIntValue,stringValue,booleanValue,nullValue,undefined]);
+
+console.log(typeof undefined);
+console.log(typeof(bigIntValue));
+
+
