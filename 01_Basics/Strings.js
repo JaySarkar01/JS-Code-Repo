57ff511298996de4e7c3a037088ab693b38ef25e
Jay Sarkar <jaisarkari9@gmail.com>
2024-10-25 18:21:50 +0000

    strings

---
+++ 01_Basics/Strings.js
@@ -0,0 +1,26 @@
+const name = "JaySarkar"
+const age = 20;
+
+// console.log(name + " is almost "+ age +" years old.")
+// console.log(`${name} is almost ${age} years old`);
+
+const gameName = new String('BGMIKhelegafreefire');
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName[3]);
+// console.log(gameName.length);
+// console.log(gameName.__proto__);
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt('2'));
+// console.log(gameName.indexOf("G"));
+// let newGame = gameName.substring(0,5)
+// console.log(newGame);
+// let nnewGame = gameName.slice(-4,7)
+// console.log(nnewGame);
+
+let myname = "    jay  "
+console.log(myname);
+console.log(myname.trim());
+
+let url = "jafjkasgfio&fFEJOISD&fAHFLSEF"
+console.log(url.replace("&f", "jiji"));
